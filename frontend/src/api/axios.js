b0732d921b8247c3b462a0d479759941f3e59990
Priya9407 <nareshpriya2007@gmail.com>
2026-03-19 20:54:11 +0530

    Connected Backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://supervisor-management-production.up.railway.app'
+  baseURL: 'https://supervisor-management-production.up.railway.app/api'
 })
 
 API.interceptors.request.use((config) => {
